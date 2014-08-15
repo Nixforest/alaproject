1c7300b19814e05582e265bfbfb0ffe4271b0f68
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-08-15 15:53:36 +0000

    [NguyenPT][Coding] Update fresh source code

---
+++ Source/AlaVui/src/com/alavui/util/Utility.java
@@ -0,0 +1,43 @@
+/*
+* Utility
+* A class provide some method for utility.
+* Version information
+*
+* Date
+* 12-Aug-2014
+* Copyright notice
+* 
+* Modification Logs:
+* DATE    AUTHOR DESCRIPTION
+*  --------------------------------------------------------
+*  12-Aug-2014	NguyenPT	Init coding
+*/
+package com.alavui.util;
+
+/**
+ * A class provide some method for utility.
+ * @author NguyenPT
+ *
+ */
+public final class Utility {
+
+	/**
+	 * Constructor.
+	 */
+	private Utility() {
+	}
+	/**
+     * Method remove sign in Vietnamese string.
+     * @param text String to remove sign
+     * @return String after remove sign
+     */
+    public static String removeSign4VietNameseString(String text) {
+        for (int i = 1; i < GlobalVariable.VIETNAMESE_SIGNS.length; i++) {
+            for (int j = 0; j < GlobalVariable.VIETNAMESE_SIGNS[i].length(); j++) {
+                text = text.replace(GlobalVariable.VIETNAMESE_SIGNS[i].charAt(j),
+                		GlobalVariable.VIETNAMESE_SIGNS[0].charAt(i - 1));
+            }
+        }
+        return text;
+    }
+}
