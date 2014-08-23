2ec22ab28f5d54755e5bc4fcc747ddc5b28e18ba
huyna88@gmail.com <huyna88@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-08-23 14:01:07 +0000

    Add script

---
+++ trunk/Source/ScriptPHP/insert_post_favorited.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    $response = array();
+    if(isset($_POST['USERID']) && isset($_POST['PID'])){
+        $userId = $_POST['USERID'];
+        $postId = $_POST['PID'];
+        // include db connect class
+        require_once __DIR__ . '/db_connect.php';
+        // connecting to db
+        $db = new DB_CONNECT();
+ 
+        // mysql inserting a new row
+        $result = mysql_query("INSERT INTO posts_favorited(USERID, PID) VALUES('$userId', '$postId')");
+ 
+        // check if row inserted or not
+        if ($result) {
+            // successfully inserted into database
+            $response["success"] = 1;
+            $response["message"] = "Post favorited is inserted successfully!";
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // failed to insert row
+            $response["success"] = 0;
+            $response["message"] = "Oops! An error occurred.";
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        }
+    } else {
+        // required field is missing
+        $response["success"] = 0;
+        $response["message"] = "Required fields is missing";
+ 
+        // echoing JSON response
+        echo json_encode($response);    
+    }
+?>
---
+++ trunk/Source/ScriptPHP/select_posts_favorited.php
@@ -0,0 +1,46 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    $response = array();
+    // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+    // connecting to db
+    $db = new DB_CONNECT();
+    if(isset($_GET['FID'])){
+	$fid = $_GET['FID'];
+
+	//Get user from user id
+	$result = mysql_query("SELECT FID, USERID, PID FROM posts_favorited WHERE FID = $fid");
+	if(!empty($result)){
+            if(mysql_num_rows($result) > 0){
+                $result = mysql_fetch_array($result);
+		$favorited = array();
+                $favorited["FID"] = $result["FID"];
+		$favorited["USERID"] = $result["USERID"];
+		$favorited["PID"] = $result["PID"];
+		
+		//Successful
+		$response["success"] = 1;	
+		$response["favorited"] = array();
+		array_push($response["favorited"], $favorited);
+		echo json_encode($response);
+            }else{
+                $response["success"] = 0;
+                $response["message"] = "No post favorited found";
+                echo json_encode($response);
+            }
+        }else{
+            $response["success"] = 0;
+            $response["message"] = "No post favorited found";
+            echo json_encode($response);
+        }
+    }else{
+	$response["success"] = 0;
+	$response["message"] = "Require field is missing";
+	echo json_encode($response);
+    }
+?>
