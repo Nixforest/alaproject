b5aff3f85f8b4bc6608deb3ae8c232bbc8fad548
huyna88@gmail.com <huyna88@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-08-23 13:49:14 +0000

    Add scripts

---
+++ trunk/Source/ScriptPHP/db_config.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define('DB_USER', "admin_sontd");
+define('DB_PASSWORD', "u0uGiPGhqZ6");
+define('DB_DATABASE', "admin_sontd");
+define("DB_SERVER", "mysql.hostinger.vn");
+?>
---
+++ trunk/Source/ScriptPHP/select_posts_reports.php
@@ -0,0 +1,47 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    $response = array();
+    // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+    // connecting to db
+    $db = new DB_CONNECT();
+    if(isset($_GET['RID'])){
+	$rid = $_GET['RID'];
+
+	//Get user from user id
+	$result = mysql_query("SELECT RID, PID, time, ip, reason FROM posts_reports WHERE RID = $rid");
+	if(!empty($result)){
+            if(mysql_num_rows($result) > 0){
+                $result = mysql_fetch_array($result);
+		$postReport = array();
+                $postReport["RID"] = $result["RID"];
+		$postReport["PID"] = $result["PID"];
+		$postReport["time"] = $result["time"];
+                $postReport["ip"] = $result["ip"];
+                $postReport["reason"] = $result["reason"];
+		
+		//Successful
+		$response["success"] = 1;	
+		$response["postreport"] = array();
+		array_push($response["postreport"], $postReport);
+		echo json_encode($response);
+            }else{
+                $response["success"] = 0;
+                $response["message"] = "No post report found";
+                echo json_encode($response);
+            }
+        }else{
+            $response["success"] = 0;
+            $response["message"] = "No post report found";
+            echo json_encode($response);
+        }
+    }else{
+	$response["success"] = 0;
+	$response["message"] = "Require field is missing";
+	echo json_encode($response);
+    }
+?>
