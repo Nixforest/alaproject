84fbc709934b7f7460e41a4387e6760232e2e427
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-08-24 16:31:48 +0000

    [NguyenPT][Coding] Update Controller class: get data.

--- trunk/Source/AlaVui/src/com/alavui/controller/MainController.java
+++ trunk/Source/AlaVui/src/com/alavui/controller/MainController.java
@@ -14,6 +14,21 @@
 */
 package com.alavui.controller;
 
+import java.util.List;
+
+import com.alavui.model.Advertisement;
+import com.alavui.model.BansIP;
+import com.alavui.model.Config;
+import com.alavui.model.DataAccessObject;
+import com.alavui.model.Level;
+import com.alavui.model.Member;
+import com.alavui.model.Post;
+import com.alavui.model.PostFavorited;
+import com.alavui.model.PostReport;
+import com.alavui.model.PostUnFavorited;
+import com.alavui.model.Referral;
+import com.alavui.model.Static;
+
 /**
  * Main controller.
  * @author NguyenPT
@@ -24,4 +39,72 @@ public enum MainController {
 	 * Instance of class.
 	 */
 	INSTANCE;
+	/**
+	 * Get data from database.
+	 * @param c Class object.
+	 * @param numberOfRow Number of record want to get.
+	 * @return List of object.
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> List<T> getData(Class<T> c, int numberOfRow) {
+		// Return null
+		List<T> listObj = null;
+		// Check if parameter is Advertisement class
+		if (c.equals(Advertisement.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListAdvertisement(numberOfRow);
+		}
+		// Check if parameter is BansIP class
+		if (c.equals(BansIP.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListBansIP(numberOfRow);
+		}
+		// Check if parameter is Config class
+		if (c.equals(Config.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListConfig(numberOfRow);
+		}
+		// Check if parameter is Level class
+		if (c.equals(Level.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListLevel(numberOfRow);
+		}
+		// Check if parameter is Member class
+		if (c.equals(Member.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListMember(numberOfRow);
+		}
+		// Check if parameter is Post class
+		if (c.equals(Post.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListPost(numberOfRow);
+		}
+		// Check if parameter is PostFavorited class
+		if (c.equals(PostFavorited.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListPostFavorited(numberOfRow);
+		}
+		// Check if parameter is PostReport class
+		if (c.equals(PostReport.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListPostReport(numberOfRow);
+		}
+		// Check if parameter is PostUnFavorited class
+		if (c.equals(PostUnFavorited.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListPostUnFavorited(numberOfRow);
+		}
+		// Check if parameter is Referral class
+		if (c.equals(Referral.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListReferral(numberOfRow);
+		}
+		// Check if parameter is Static class
+		if (c.equals(Static.class)) {
+			listObj = (List<T>)DataAccessObject.
+					INSTANCE.getListStatic(numberOfRow);
+		}
+		// Return value
+		return listObj;
+	}
 }
