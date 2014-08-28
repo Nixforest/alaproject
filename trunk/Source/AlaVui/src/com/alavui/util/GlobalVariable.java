a43c28862dbed73924a0d13af3482477709f7733
huyna88@gmail.com <huyna88@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-08-28 15:28:19 +0000

    Add constants url

--- trunk/Source/AlaVui/src/com/alavui/util/GlobalVariable.java
+++ trunk/Source/AlaVui/src/com/alavui/util/GlobalVariable.java
@@ -80,7 +80,49 @@ public final class GlobalVariable {
 	/**
 	 * String array to remove sign in Vietnamese.
 	 */
-	 public static final String[] VIETNAMESE_SIGNS = new String[]{"aAeEoOuUiIdDyY", "áàạảãâấầậẩẫăắằặẳẵ", "ÁÀẠẢÃÂẤẦẬẨẪĂẮẰẶẲẴ",
-         "éèẹẻẽêếềệểễ", "ÉÈẸẺẼÊẾỀỆỂỄ", "óòọỏõôốồộổỗơớờợởỡ", "ÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠ", "úùụủũưứừựửữ", "ÚÙỤỦŨƯỨỪỰỬỮ",
-         "íìịỉĩ", "ÍÌỊỈĨ", "đ", "Đ", "ýỳỵỷỹ", "ÝỲỴỶỸ"};
+	 public static final String[] VIETNAMESE_SIGNS = new String[]{"aAeEoOuUiIdDyY", "Ã¡Ã áº¡áº£Ã£Ã¢áº¥áº§áº­áº©áº«Äƒáº¯áº±áº·áº³áºµ", "Ã�Ã€áº áº¢ÃƒÃ‚áº¤áº¦áº¬áº¨áºªÄ‚áº®áº°áº¶áº²áº´",
+         "Ã©Ã¨áº¹áº»áº½Ãªáº¿á»�á»‡á»ƒá»…", "Ã‰Ãˆáº¸áººáº¼ÃŠáº¾á»€á»†á»‚á»„", "Ã³Ã²á»�á»�ÃµÃ´á»‘á»“á»™á»•á»—Æ¡á»›á»�á»£á»Ÿá»¡", "Ã“Ã’á»Œá»ŽÃ•Ã”á»�á»’á»˜á»”á»–Æ á»šá»œá»¢á»žá» ", "ÃºÃ¹á»¥á»§Å©Æ°á»©á»«á»±á»­á»¯", "ÃšÃ™á»¤á»¦Å¨Æ¯á»¨á»ªá»°á»¬á»®",
+         "Ã­Ã¬á»‹á»‰Ä©", "Ã�ÃŒá»Šá»ˆÄ¨", "Ä‘", "Ä�", "Ã½á»³á»µá»·á»¹", "Ã�á»²á»´á»¶á»¸"};
+	 
+	 /*
+	  * String link get all members
+	  */
+	 public static final String URL_MEMBER_ALL				= "";
+	 /*
+	  * String link get member by Id
+	  */
+	 public static final String URL_MEMBER_DETAIL			= "";
+	 /*
+	  * String link get all posts
+	  */
+	 public static final String URL_POST_ALL				= "";
+	 /*
+	  * String link get post by Id
+	  */
+	 public static final String URL_POST_DETAIL				= "";
+	 /*
+	  * String link get post favorited all
+	  */
+	 public static final String URL_POST_FAVORITED_ALL		= "";
+	 /*
+	  * String link get post favorited by Id
+	  */
+	 public static final String URL_POST_FAVORITED_DETAIL	= "";
+	 /*
+	  * String link get post report all
+	  */
+	 public static final String URL_POST_REPORT_ALL			= "";
+	 /*
+	  * String link get post report detail by Id
+	  */
+	 public static final String URL_POST_REPORT_DETAIL		= "";
+	 /*
+	  * String link get post unfavorited all
+	  */
+	 public static final String URL_POST_UNFAVORITED_ALL	= "";
+	 /*
+	  * String link get post unfavorited by Id
+	  */
+	 public static final String URL_POST_UNFAVORITE_DETAIL	= "";
+	 
 }
