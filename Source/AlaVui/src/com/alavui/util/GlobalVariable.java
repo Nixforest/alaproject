ffdbcee5367c0b3b4e9b748758be2e349e09bb4d
huyna88@gmail.com <huyna88@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-09-01 14:29:30 +0000

    Implement DataAccess

--- Source/AlaVui/src/com/alavui/util/GlobalVariable.java
+++ Source/AlaVui/src/com/alavui/util/GlobalVariable.java
@@ -127,6 +127,10 @@ public final class GlobalVariable {
 	  * String link get member by Id.
 	  */
 	 public static final String URL_MEMBER_DETAIL			= "";
+	 /*
+	  * String link insert member
+	  */
+	 public static final String URL_MEMBER_INSERT			= "";
 	 /**
 	  * String link get all posts.
 	  */
@@ -171,7 +175,32 @@ public final class GlobalVariable {
 	 public static final String EMAIL						= "email";
 	 public static final String USER_NAME					= "username";
 	 public static final String PASSWORD					= "passwrord";
+	 public static final String PWD							= "pwd";
 	 public static final String FULLNAME					= "fullname";
+	 public static final String GENDER						= "gender";
+	 public static final String DESCRIPTION					= "description";
+	 public static final String COUNTRY						= "country";
+	 public static final String YOUR_VIEWED					= "yourviewed";
+	 public static final String PROFILE_VIEWS				= "profileviews";
+	 public static final String YOU_VIEWED					= "youviewed";
+	 public static final String ADD_TIME					= "addtime";
+	 public static final String LAST_LOGIN					= "lastlogin";
+	 public static final String VERIFIED					= "verified";
+	 public static final String STATUS						= "status";
+	 public static final String PROFILE_PICTURE				= "profilepicture";
+	 public static final String REMEMBER_ME_KEY				= "remember_me_key";
+	 public static final String REMEMBER_ME_TIME			= "remember_me_time";
+	 public static final String IP							= "ip";
+	 public static final String LIP							= "lip";
+	 public static final String WEBSITE						= "website";
+	 public static final String NEWS						= "news";
+	 public static final String MY_LANG						= "mylang";
+	 public static final String COLOR_1						= "color1";
+	 public static final String COLOR_2						= "color2";
+	 public static final String FILTER						= "filter";
+	 public static final String POINTS						= "points";
+	 public static final String TWITTER_ID					= "twitter_id";
+	 public static final String COVER						= "cover";
 	 
 	 /**
 	  * Table posts and fields.
@@ -193,4 +222,9 @@ public final class GlobalVariable {
 	  */
 	 public static final String TABLE_POST_FAVORITED		= "posts_favorited";
 	 public static final String FAVORITED_ID				= "FID";
+	 
+	 public static final String POSTS_REPORTS				= "posts_reports";
+	 public static final String REPORT_ID					= "RID";
+	 public static final String REPORT_TIME					= "time";
+	 public static final String REPORT_REASON				= "reason";
 }
