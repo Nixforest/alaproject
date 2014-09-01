d5a827b7a820104e1af18d20299314f9dc715bcc
huyna88@gmail.com <huyna88@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-09-01 14:27:01 +0000

    Add script PHP

---
+++ trunk/Source/ScriptPHP/select_all_posts_report.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    $response = array();
+    // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+    // connecting to db
+    $db = new DB_CONNECT();
+ 
+    // mysql select all
+    $result = mysql_query("SELECT RID, PID, time, ip, reason FROM posts_reports");
+ 
+    // check for empty result
+    if (mysql_num_rows($result) > 0) {
+        // Post node
+        $response["posts_reports"] = array();
+
+        while ($row = mysql_fetch_array($result)) {
+            // temp user array
+            $postReports = array();
+            $postReports["RID"]      = $result["RID"];
+            $postReports["PID"]      = $result["PID"];
+            $postReports["time"]     = $result["time"];
+            $postReports["ip"]       = $result["ip"];
+            $postReports["reason"]   = $result["reason"];
+            // push single product into final response array
+            array_push($response["posts_reports"], $postReports);
+        }
+        // success
+        $response["success"] = 1;
+        // echo JSON response
+        echo json_encode($response);
+    } else {
+        // no users found
+	$response["success"] = 0;
+	$response["message"] = "No records";
+	// echo no users JSON
+	echo json_encode($response);
+    }
+?>
