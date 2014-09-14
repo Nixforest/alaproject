b03fd4051774b6eeebdcca13a6d5ad08123a5226
huyna88@gmail.com <huyna88@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-09-14 14:52:04 +0000

    Update scripts update Post

--- trunk/Source/ScriptPHP/update_posts_favclicks.php
+++ trunk/Source/ScriptPHP/update_posts_favclicks.php
@@ -6,40 +6,51 @@
  * and open the template in the editor.
  */
     $response = array();
-    if(isset($_POST['PID']) && isset($_POST['favclicks'])){
-		$pid            = $_POST['PID'];
-		$favclicks      = $_POST['favclicks'];
+    if(isset($_POST['PID'])){
+		$pid = $_POST['PID'];
 		
         // include db connect class
         require_once __DIR__ . '/db_connect.php';
         // connecting to db
         $db = new DB_CONNECT();
- 
-        // mysql inserting a new row
-        $result = mysql_query("UPDATE posts SET favclicks = $favclicks WHERE PID = $pid");
+		$click = 0;
+		// Get current favClicks
+		$favclicks = mysql_query("SELECT favclicks FROM posts WHERE PID = $pid");
+		
+		if(!empty($favclicks)){
+			if(mysql_num_rows($favclicks) > 0){
+				$favclicks = mysql_fetch_array($favclicks);
+				$click = $favclicks['favclicks'] + 1;
+
+				// mysql inserting a new row
+				$result = mysql_query("UPDATE posts SET favclicks = $click WHERE PID = $pid");
             
-        // check if row inserted or not
-        if ($result) {
-            // successfully inserted into database
-            $response["success"] = 1;
-            $response["message"] = "Post is updated successfully!";
- 
-            // echoing JSON response
-            echo json_encode($response);
-        } else {
-            // failed to insert row
-            $response["success"] = 0;
-            $response["message"] = "Oops! An error occurred.";
- 
-            // echoing JSON response
-            echo json_encode($response);
-        }
+				// check if row inserted or not
+				if ($result) {
+					// successfully inserted into database
+					$response["success"] = 1;
+					$response["message"] = "Post is updated successfully!";
+					// echoing JSON response
+					echo json_encode($response);
+				} else {
+					// failed to insert row
+					$response["success"] = 0;
+					$response["message"] = "Oops! An error occurred.";
+					// echoing JSON response
+					echo json_encode($response);
+				}
+			}
+		} else{
+				$response["success"] = 0;
+				$response["message"] = "FavClicks is no record!";
+				echo json_encode($response);
+		}
     } else {
-        // required field is missing
+		// required field is missing
         $response["success"] = 0;
         $response["message"] = "Required fields is missing";
  
-        // echoing JSON response
-        echo json_encode($response);    
+		//echoing JSON response
+       echo json_encode($response);    
     }
 ?>
