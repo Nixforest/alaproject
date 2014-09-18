57ec015fe94fc1caf2b460ab792c858f0ba9b845
tamnt2692@gmail.com <tamnt2692@gmail.com@e88b1771-f820-ccb1-3f4d-55ccf13bc118>
2014-09-18 17:09:34 +0000

    [TamNT][Code] update code

--- Source/AlaVui/src/com/alavui/ui/SplashScreenActivity.java
+++ Source/AlaVui/src/com/alavui/ui/SplashScreenActivity.java
@@ -1,17 +1,13 @@
 package com.alavui.ui;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
 
-public class SplashScreenActivity extends ActionBarActivity {
+public class SplashScreenActivity extends Activity {
 
 	private static int SPLASH_TIME_OUT = 1000;
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
@@ -34,24 +30,5 @@ public class SplashScreenActivity extends ActionBarActivity {
 	                finish();
 	            }
 	        }, SPLASH_TIME_OUT);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash_screen, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
